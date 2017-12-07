841c93d5789393b61790387be8dd09519bbcfdbf
Matt Noonan <matt.noonan@gmail.com>
2017-12-07 15:18:54 -0500

    readme

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
-* Data messages
+* The idea
 
-A data message contains the sender's node id N, a timestamp T,
-and a random number 0 <= R <= 1.
+Each node $n$ in the network maintains a set of *seen* messages $S_n$, and a set of
+*canonical* messages $C_n$. During the initial phase, all nodes send messages with
+random payloads to each other, which are accumulated into the *seen* sets.
 
-Data messages are broadcast to all nodes.
+The *canonical* messages $C_n$ have the important invariant that
+$x \in C_n$ if and only if $x$ is in $C_m$ for at least half of the nodes in the
+network.
+It follows that for any subset $X \subseteq N$ with $|X| > \frac{|N|}{2}$,
+$$ \bigcup_{x \in X} C_x = \bigcup_{n \in N} C_n $$
 
-* Worker state
-
-Each worker maintains the following internal state:
-
-  - The set of messages S received so far.
-  - The "canonical" set C of messages agreed on with the other nodes.
-
-From this, we can also derive the delta (G,K) between C and S, such that
-S = (C \ K) + G. We can assume that G and K have an empty intersection.
-
-The idea here is that each worker keeps track of the data messages it has seen,
-and also works to create a consensus with other nodes about what the "correct"
-set of messages is. 
+Periodically, a helper checks if $S_k \setminus C_k$ is non-empty. If so, 
--- src/Network/Worker.hs
+++ src/Network/Worker.hs
@@ -34,35 +34,6 @@ import Data.Maybe
 import Data.Binary.Orphans
 import Data.List (foldl')
 
-data Msg = Msg { payload :: Double
-               , sender  :: ProcessId
-               , timestamp :: UTCTime } deriving (Eq, Show, Generic)
-
-instance Ord Msg where
-  compare x y = compare (timestamp x, sender x, payload x) (timestamp y, sender y, payload y)
-  
-instance Binary Msg
-
-(%=) :: MonadIO m => MVar a -> (a -> a) -> m a
-mvar %= f = liftIO (modifyMVar mvar (\x -> let x' = f x in return (x',x')))
-
-inspect :: MonadIO m => MVar a -> m a
-inspect mvar = liftIO (readMVar mvar)
-
-accumulate :: MVar (Set Msg) -> Process ()
-accumulate seen = forever (receiveWait [ match (\msg -> seen %= S.insert msg) ])
-    
-yakker :: Config -> (Msg -> Process ()) -> Process ()
-yakker config netsend = do
-  self <- getSelfPid
-  
-  forever $ do
-    now <- liftIO getCurrentTime
-    x   <- liftIO (rng config)
-    netsend (Msg { payload   = x,
-                   sender    = self,
-                   timestamp = now })
-
 -- | Execute the message-sending and agreement phases, and display the result.
 iohk :: Config -> Process ()
 iohk config = do
@@ -122,23 +93,57 @@ work config = do
     -- Send everybody our vote for the final result
     kill writerPid "time to vote"
     canon <- liftIO (takeMVar canonical)
-    broadcastTo net "answer" (Vote canon self)
+    broadcastTo net "answer" (Vote canon)
     
     -- Once the votes are in, return the result.
     ans <- liftIO (takeMVar answer)
     return (S.size ans, sum (zipWith (*) (map payload $ S.toList ans) [1..]))
     
 
+-- | A message containing a random number.
+data Msg = Msg { payload :: Double
+               , sender  :: ProcessId
+               , timestamp :: UTCTime } deriving (Eq, Show, Generic)
+
+instance Ord Msg where
+  compare x y = compare (timestamp x, sender x, payload x) (timestamp y, sender y, payload y)
+  
+instance Binary Msg
+
+-- | Receive random numbers and add them to the "seen" set.
+--   This is the "iohk-test" named process.
+accumulate :: MVar (Set Msg) -> Process ()
+accumulate seen = forever (receiveWait [ match (\msg -> seen %= S.insert msg) ])
+
+-- | Continuously send random numbers to the network.
+yakker :: Config -> (Msg -> Process ()) -> Process ()
+yakker config netsend = do
+  self <- getSelfPid
+  
+  forever $ do
+    now <- liftIO getCurrentTime
+    x   <- liftIO (rng config)
+    netsend (Msg { payload   = x,
+                   sender    = self,
+                   timestamp = now })
+
+-- | A unique ID to use for writes. This is used to ensure that each
+--   write is acknowledged by a quorum before committing it.
 newtype Round = Round Int deriving (Eq, Show, Generic)
 instance Binary Round
 
+-- | A request to add the given delta to a node's canonical set.
 data RequestWrite = RequestWrite { writeId :: Round, writer :: ProcessId, delta :: Set Msg }
   deriving (Show, Generic)
 instance Binary RequestWrite
 
+-- | Acknowledgement of a request to write.
 data Ack = Ack Round ProcessId deriving (Eq, Show, Generic)
 instance Binary Ack
 
+-- | This worker task periodically checks if this node has seen
+--   any messages that are not yet canonical; if so, it broadcasts
+--   a request to make the new messages canonical.
 requestWrite :: (RequestWrite -> Process ())
              -> Int
              -> MVar (Set Msg)
@@ -158,7 +163,8 @@ requestWrite netsend quorumSize seen canonical = forM_ [0..] $ \k -> do
 
   -- Pause for 1/10 of a second
   liftIO (threadDelay 100000)
-  
+
+-- | Ask a majority of the network to accept this write.
 write :: (RequestWrite -> Process ())
       -> Int
       -> Round
@@ -182,9 +188,12 @@ write netsend quorumSize wid vs = do
   --      guarantee that the write will be visible on any majority.
   forM_ quorum $ \peer -> send peer Commit
 
+-- | Tell a member of the quorum to accept the write we proposed.
 data Commit = Commit deriving (Show, Generic)
 instance Binary Commit
 
+-- | Accept a write request, provisionally at first, committing the write
+--   when a @Commit@ message is received.
 applyWrite :: MVar (Set Msg) -> Process ()
 applyWrite canonical = forever (receiveWait [ match go ])
   where
@@ -196,15 +205,29 @@ applyWrite canonical = forever (receiveWait [ match go ])
                                                               return () ) ])
       send (writer req) (Ack (writeId req) commit)
       
-      
-data Vote = Vote (Set Msg) ProcessId deriving (Show, Generic)
+-- | Vote for a given set.     
+newtype Vote = Vote (Set Msg) deriving (Show, Generic)
 instance Binary Vote
 
-data AckVote = AckVote deriving (Show, Generic)
-instance Binary AckVote
-
+-- | Gather votes for the canonical set, and return the union of all votes.
+--   Senders should only be voting for a set if they have accepted it as
+--   their canonical set, and every element in a canonical set should belong
+--   to the canonical set of some majority of nodes. Hence, if we get back
+--   a quorum of votes, then union over the votes is the same as the union
+--   over the canonical sets of all nodes.
+--
+--   (but see BUG in the @write@ function)
 tallyVotes :: MVar (Set Msg) -> Int -> Process ()
 tallyVotes answer quorumSize = do
   self <- getSelfPid
-  ans <- replicateM quorumSize $ receiveWait [ match (\(Vote x _) -> return x) ]
+  ans <- replicateM quorumSize $ receiveWait [ match (\(Vote x) -> return x) ]
   liftIO $ putMVar answer (S.unions ans)
+
+-- | Modify an MVar using the given function, and return the new value.
+(%=) :: MonadIO m => MVar a -> (a -> a) -> m a
+mvar %= f = liftIO (modifyMVar mvar (\x -> let x' = f x in return (x',x')))
+
+-- | Read the value in an MVar.
+inspect :: MonadIO m => MVar a -> m a
+inspect mvar = liftIO (readMVar mvar)
+
